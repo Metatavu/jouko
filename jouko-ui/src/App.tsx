25976d324f56b83270fa502790565743884a7e2f
haxhaagn15 <agnesa.haxha@edu.fh-joanneum.at>
2018-04-04 14:57:54 +0300

    Added react-router

--- jouko-ui/src/App.tsx
+++ jouko-ui/src/App.tsx
@@ -1,20 +1,30 @@
 import * as React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        <BrowserRouter>
+            <div>
+            <div className="Header">
+                <Header />
+            </div>
+
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.tsx</code> and save to reload.
+            </p>
+          </div>
+            </div>
+        </BrowserRouter>
     );
   }
 }
