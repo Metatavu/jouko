1ed930aa67935c75399699ee513cdd2a73527ed8
Ilmo Euro <ilmo.euro@gmail.com>
2018-04-04 12:41:28 +0300

    UI component basics

--- jouko-ui/src/App.tsx
+++ jouko-ui/src/App.tsx
@@ -1,9 +1,91 @@
 import * as React from 'react';
 import './App.css';
+import { InterruptionGroupsApi, InterruptionGroup } from 'jouko-ts-client';
 
 // const logo = require('./logo.svg');
 
-class App extends React.Component {
+interface InterruptionGroupTableRowProps {
+  entityId: Number;
+  startDate: Date;
+  endDate: Date;
+}
+
+class InterruptionGroupsTableRow extends React.Component<InterruptionGroupTableRowProps, {}> {
+  constructor(props: InterruptionGroupTableRowProps) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props);
+
+    return (
+      <tr>
+        <td>{this.props.entityId}</td>
+        <td>{this.props.startDate.toISOString()}</td>
+        <td>{this.props.endDate.toISOString()}</td>
+      </tr>
+    );
+  }
+}
+
+interface InterruptionGroupTableProps {
+  rowProps: InterruptionGroupTableRowProps[];
+}
+
+class InterruptionGroupsTable extends React.Component<InterruptionGroupTableProps> {
+  constructor(props: InterruptionGroupTableProps) {
+    super(props);
+  }
+
+  render() {
+    const rows = [];
+    for (const rowProp of this.props.rowProps) {
+      rows.push(
+        <InterruptionGroupsTableRow
+          key={rowProp.entityId.toString()}
+          entityId={rowProp.entityId}
+          startDate={rowProp.startDate}
+          endDate={rowProp.endDate}
+        />
+      );
+    }
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>startDate</th>
+            <th>endDate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+interface AppState {
+  interruptionGroups: InterruptionGroup[];
+}
+
+class App extends React.Component<{}, AppState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {interruptionGroups: []};
+  }
+
+  componentDidMount() {
+    const api = new InterruptionGroupsApi(undefined, 'http://192.168.100.14:8080/api-0.0.1-SNAPSHOT/v1');
+
+    api.listInterruptionGroups(0, 10000).then(groups => {
+      this.setState({interruptionGroups: groups});
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,27 +98,16 @@ class App extends React.Component {
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       */}
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>startDate</th>
-            <th>endDate</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>2018-04-04T11:11:33+03:00</td>
-            <td>2018-04-04T12:11:33+03:00</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>2018-04-04T15:11:33+03:00</td>
-            <td>2018-04-04T16:11:33+03:00</td>
-          </tr>
-        </tbody>
-      </table>
+        <InterruptionGroupsTable
+          rowProps={this.state.interruptionGroups.map(group => {
+            console.log(group);
+            return {
+              entityId: group.id,
+              startDate: new Date(group.startTime),
+              endDate: new Date(group.endTime)};
+            })
+          }
+        />
       </div>
     );
   }
