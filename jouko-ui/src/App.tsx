a37d5866fb42e51410b369fecf73393e2a5f7544
haxhaagn15 <agnesa.haxha@edu.fh-joanneum.at>
2018-04-23 09:21:21 +0300

    fix that content should be displayed before user logs in

--- jouko-ui/src/App.tsx
+++ jouko-ui/src/App.tsx
@@ -19,6 +19,7 @@ const logo = require('./logo.svg');
 interface AppState {
   keycloakInstance?: Keycloak.KeycloakInstance;
   username?: Keycloak.KeycloakInstance;
+  userId?: Keycloak.KeycloakInstance;
 }
 
 class App extends React.Component<{}, AppState> {
@@ -38,8 +39,12 @@ class App extends React.Component<{}, AppState> {
     kc.init({ onLoad: 'login-required' })
       .success(() => {
       // tslint:disable-next-line:no-any
-      this.setState({keycloakInstance : kc, username: (kc.idTokenParsed as any).name });
-      }
+      this.setState({keycloakInstance : kc, username: (kc.idTokenParsed as any).name,
+        // tslint:disable-next-line:no-any
+        userId: (kc.idTokenParsed as any).sub });
+      // tslint:disable-next-line:no-any
+      // console.log(kc.idTokenParsed as any);
+    }
     )
       .error((e) => {console.log(e); } );
   }
@@ -51,39 +56,51 @@ class App extends React.Component<{}, AppState> {
   }
 
   render() {
+    let content;
+    if (!this.state.keycloakInstance) {
+      content = '';
+    } else {
+      content = (
+        <div>
+          <div className="Navigationbar">
+            <div className="Navigation">
+              <Header logout={() => this.logout()}/>
+            </div>
+            <div className="Logout">
+              <NavLink to="/" onClick={() => this.logout()}>
+                <i className="fa fa-sign-out"/>
+              </NavLink>
+            </div>
+          </div>
+          <div className="App-Block1">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">JOUKO - kotiapp</h1>
+        <h1>Kirjautuneena: {this.state.username} </h1>
+        <p>User ID: {this.state.userId} </p>
+      </div>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/User" component={User} />
+        <Route path="/Settings" component={Settings} />
+        <Route
+          path="/Statistics/:id"
+          render={props => (
+            <Statistics deviceId={props.match.params.id as number}/>
+          )}
+        />
+        <Route path="/UpcomingInterruptions" component={UpcomingInterruptions} />
+        <Route path="/InterruptionGroupsTable" component={InterruptionGroupsTable} />
+        <Route path="/PowerUsageSummary" component={PowerUsageSummary} />
+        <div className="Bottombar">
+          <Bottombar />
+        </div>
+      </div>
+      );
+    }
+
     return (
         <BrowserRouter>
             <div className="wrapper">
-                <div className="Navigationbar">
-                  <div className="Navigation">
-                    <Header logout={() => this.logout()}/>
-                  </div>
-                  <div className="Logout">
-                    <NavLink to="/" onClick={() => this.logout()}>
-                      <i className="fa fa-sign-out"/>
-                    </NavLink>
-                  </div>
-                </div>
-                <div className="App-Block1">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <h1 className="App-title">JOUKO - kotiapp</h1>
-                  <h1>Kirjautuneena: {this.state.username} </h1>
-                </div>
-                <Route path="/" exact={true} component={Home} />
-                <Route path="/User" component={User} />
-                <Route path="/Settings" component={Settings} />
-                <Route
-                  path="/Statistics/:id"
-                  render={props => (
-                    <Statistics deviceId={props.match.params.id as number}/>
-                  )}
-                />
-                <Route path="/UpcomingInterruptions" component={UpcomingInterruptions} />
-                <Route path="/InterruptionGroupsTable" component={InterruptionGroupsTable} />
-                <Route path="/PowerUsageSummary" component={PowerUsageSummary} />
-                <div className="Bottombar">
-                  <Bottombar />
-                </div>
+              {content}
             </div>
         </BrowserRouter>
     );
