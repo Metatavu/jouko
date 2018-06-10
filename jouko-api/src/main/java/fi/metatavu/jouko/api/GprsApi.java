bd3119bea9c56ff80627bf62085c0f700339ecf5
Ilmo Euro <ilmo.euro@gmail.com>
2018-06-10 15:44:28 +0300

    LoRa 2-way comm

--- jouko-api/src/main/java/fi/metatavu/jouko/api/GprsApi.java
+++ jouko-api/src/main/java/fi/metatavu/jouko/api/GprsApi.java
@@ -1,5 +1,6 @@
 package fi.metatavu.jouko.api;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
@@ -11,20 +12,23 @@ import java.util.regex.Pattern;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.management.RuntimeErrorException;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
-import org.jboss.resteasy.util.Hex;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import fi.metatavu.jouko.api.device.Laiteviestit.AikasynkLaitteelle;
@@ -38,6 +42,7 @@ import fi.metatavu.jouko.api.model.MeasurementType;
 
 @Stateless
 @Path("/gprs")
+// TODO rename
 public class GprsApi {
   
   @Inject
@@ -51,8 +56,6 @@ public class GprsApi {
   
   private static final Pattern MESSAGE_PATTERN = Pattern.compile("\\{([^}]*)\\}");
   
-  private static final Pattern AUTH_PATTERN = Pattern.compile("^Key ([a-zA-Z0-9]+)$");
-  
   private static class InvalidDeviceException extends Exception {
     public InvalidDeviceException(String msg) {
       super(msg);
@@ -61,15 +64,140 @@ public class GprsApi {
     private static final long serialVersionUID = 8322806559664622171L;
   }
   
+  private static class Envelope {
+    private DevEuiUplink devEuiUplink;
+
+    @JsonProperty("DevEUI_uplink")
+    public DevEuiUplink getDevEuiUplink() {
+      return devEuiUplink;
+    }
+
+    @JsonProperty("DevEUI_uplink")
+    public void setDevEuiUplink(DevEuiUplink devEuiUplink) {
+      this.devEuiUplink = devEuiUplink;
+    }
+  }
+  
+  @JsonIgnoreProperties(ignoreUnknown=true)
+  private static class DevEuiUplink {
+    private String customerId;
+    private String devEui;
+    private int fPort;
+    private int fCntUp;
+    private String payloadHex;
+
+    @JsonProperty("CustomerID")
+    public String getCustomerId() {
+      return customerId;
+    }
+
+    @JsonProperty("CustomerID")
+    public void setCustomerId(String customerId) {
+      this.customerId = customerId;
+    }
+
+    @JsonProperty("DevEUI")
+    public String getDevEui() {
+      return devEui;
+    }
+
+    @JsonProperty("DevEUI")
+    public void setDevEui(String devEui) {
+      this.devEui = devEui;
+    }
+    
+    @JsonProperty("FPort")
+    public int getFPort() {
+      return fPort;
+    }
+    
+    @JsonProperty("FPort")
+    public void setFPort(int fPort) {
+      this.fPort = fPort;
+    }
+
+    @JsonProperty("FCntUp")
+    public int getFCntUp() {
+      return fCntUp;
+    }
+
+    @JsonProperty("FCntUp")
+    public void setFCntUp(int fCntUp) {
+      this.fCntUp = fCntUp;
+    }
+
+    @JsonProperty("payload_hex")
+    public String getPayloadHex() {
+      return payloadHex;
+    }
+
+    @JsonProperty("payload_hex")
+    public void setPayloadHex(String payloadHex) {
+      this.payloadHex = payloadHex;
+    }
+  }
+  
   @POST
   @Path("/lora/")
   public Response communicateWithLoraDevice(
-      @FormParam("payload") String payload
+    Envelope envelope,
+    @QueryParam("AS_ID") String asId,
+    @QueryParam("LrnDevEui") String lrnDevEui,
+    @QueryParam("LrnFPort") String lrnFPort,
+    @QueryParam("LrnInfos") String lrnInfos,
+    @QueryParam("Time") String time,
+    @QueryParam("Token") String token
   ) {
-    byte[] unhex = Hex.decodeHex(payload);
-    return null;
+    DevEuiUplink uplink = envelope.getDevEuiUplink();
+    String payloadHex = uplink.getPayloadHex();
+    String message;
+    try {
+      message = new String(Hex.decodeHex(payloadHex.toCharArray()),
+                                  StandardCharsets.US_ASCII);
+    } catch (DecoderException e) {
+      logger.error("Invalid hex string: {}", payloadHex);
+      return Response.status(400).entity("Invalid hex string").build();
+    }
+    String bodyParameters = String.format("%s%s%d%d%s",
+        uplink.getCustomerId(),
+        uplink.getDevEui(),
+        uplink.getFPort(),
+        uplink.getFCntUp(),
+        uplink.getPayloadHex());
+    String queryParameters = String.format(
+        "LrnDevEui=%s&LrnFPort=%s&LrnInfos=%s&AS_ID=%s&Time=%s",
+         lrnDevEui,   lrnFPort,   lrnInfos,   asId,    time);
+    String hashedStringNoKey = bodyParameters + queryParameters;
+    logger.info("hashedStringNoKey: {}", hashedStringNoKey);
+    
+    ControllerEntity controller = deviceController.findControllerByEui(uplink.getDevEui());
+    logger.info("Eui: {}", uplink.getDevEui());
+    logger.info("Controller: {}", controller);
+    if (controller == null) {
+      return Response.status(Status.BAD_REQUEST).entity("Controller not found").build();
+    }
+
+    String hashedStringKey = hashedStringNoKey + controller.getKey();
+    String hash = DigestUtils.sha256Hex(hashedStringKey.getBytes(StandardCharsets.UTF_8));
+    logger.info("kellonaika: {}", time);
+    logger.info("hash: {}", hash);
+    logger.info("token: {}", token);
+    if (!Objects.equals(hash, token)) {
+      return Response.status(Status.BAD_REQUEST).entity("Bad token").build();
+    }
+
+    Matcher messageMatcher = MESSAGE_PATTERN.matcher(message);
+    List<String> messages = new ArrayList<>();
+    try {
+      unpackMessages(messageMatcher, messages);
+    } catch (InvalidProtocolBufferException | InvalidDeviceException ex) {
+      return Response
+          .status(400)
+          .entity(String.format("Protobuf error: %s", ex.getMessage()))
+          .build();
+    }
+    return Response.ok().build();
   }
-      
   
   @POST
   @Path("/gprs/{eui}")
@@ -77,6 +205,8 @@ public class GprsApi {
       @PathParam("eui") String eui,
       String content
   ) {
+    
+    // TODO basic auth using eui/key
 
     ControllerEntity controller = deviceController.findControllerByEui(eui);
     if (controller == null) {
@@ -85,30 +215,6 @@ public class GprsApi {
                      .build();
     }
     
-    /*
-    String authHeader = request.getHeader("Authorization");
-    if (authHeader == null) {
-      return Response.status(Status.UNAUTHORIZED)
-                     .entity("No authorization header")
-                     .build();
-    }
-    
-    Matcher authMatcher = AUTH_PATTERN.matcher(authHeader);
-    if (authMatcher.matches()) {
-      String key = authMatcher.group(1);
-      
-      if (!Objects.equals(controller.getKey(), key)) {
-        return Response.status(Status.UNAUTHORIZED)
-                       .entity("Invalid key")
-                       .build();
-      }
-    } else {
-      return Response.status(Status.UNAUTHORIZED)
-                     .entity("Invalid key format")
-                     .build();
-    }
-    
-    */
     Matcher messageMatcher = MESSAGE_PATTERN.matcher(content);
     List<String> messages = new ArrayList<>();
     try {
