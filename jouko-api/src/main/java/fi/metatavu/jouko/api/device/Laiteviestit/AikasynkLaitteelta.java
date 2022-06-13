5a65d0afb29e439efd1792b837b2d2b38cd0dd89
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-13 14:51:56 +0300

    Return system's time in epoch seconds

--- jouko-api/src/main/java/fi/metatavu/jouko/api/device/Laiteviestit/AikasynkLaitteelta.java
+++ jouko-api/src/main/java/fi/metatavu/jouko/api/device/Laiteviestit/AikasynkLaitteelta.java
@@ -1,4 +1,8 @@
 package fi.metatavu.jouko.api.device.Laiteviestit;
 
 public class AikasynkLaitteelta {
+    public long getLaiteaika() {
+        // Return the current time in Epoch seconds
+        return System.currentTimeMillis() / 1000;
+    }
 }
