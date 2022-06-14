ee20ccb49e091a89ad0734f3d460c31562280e58
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-14 08:14:40 +0300

    Add missing files and optimise imports

--- jouko-api/src/main/java/fi/metatavu/jouko/api/dao/InterruptionDAO.java
+++ jouko-api/src/main/java/fi/metatavu/jouko/api/dao/InterruptionDAO.java
@@ -1,23 +1,12 @@
 package fi.metatavu.jouko.api.dao;
 
-import java.time.OffsetDateTime;
-import java.util.List;
+import fi.metatavu.jouko.api.model.*;
 
 import javax.enterprise.context.Dependent;
 import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
-
-import fi.metatavu.jouko.api.model.DeviceEntity;
-import fi.metatavu.jouko.api.model.GprsMessageEntity;
-import fi.metatavu.jouko.api.model.GprsMessageEntity_;
-import fi.metatavu.jouko.api.model.InterruptionEntity;
-import fi.metatavu.jouko.api.model.InterruptionEntity_;
-import fi.metatavu.jouko.api.model.InterruptionGroupEntity;
-import fi.metatavu.jouko.api.model.InterruptionGroupEntity_;
+import javax.persistence.criteria.*;
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @Dependent
 public class InterruptionDAO extends AbstractDAO<InterruptionEntity> {
