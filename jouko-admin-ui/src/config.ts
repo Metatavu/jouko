bac9c74cd66b6f4da4cdacf531deeee595f1c0f6
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-17 09:02:28 +0300

    Add config doc for admin-ui as well, forgot

--- jouko-admin-ui/src/config.ts
+++ jouko-admin-ui/src/config.ts
@@ -14,11 +14,13 @@ export const uploadUrl =
   ? 'https://jouko-api.metatavu.io/fileUpload'
   : 'http://dev.jouko.fi:8080/fileUpload';
 
+// Keycloak realm name, if you followed the README instructions, this should be "jouko-realm"
 export const keycloakRealm =
   process.env.NODE_ENV === 'production'
   ? 'jouko-realm'
   : 'jouko-realm';
 
+// Keycloak ClientID for the application, if you followed the README instructions, this should be "jouko-api"
 export const keycloakClientId =
   process.env.NODE_ENV === 'production'
   ? 'jouko-admin'
