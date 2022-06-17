d9b2f9f759479b4d68e469f1eecfbee5928c3660
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-17 13:58:38 +0300

    Create TimeSyncDAO?

--- jouko-api/src/main/java/fi/metatavu/jouko/api/model/TimeSyncEntity.java
+++ jouko-api/src/main/java/fi/metatavu/jouko/api/model/TimeSyncEntity.java
@@ -10,7 +10,20 @@ public class TimeSyncEntity {
   @Column(nullable = false)
   @NotNull
   private OffsetDateTime time;
+  private DeviceEntity device;
   
   @NotNull
   private int reason;
+
+    public void setTime(OffsetDateTime time) {
+        this.time = time;
+    }
+
+    public void setDevice(DeviceEntity device) {
+        this.device = device;
+    }
+
+    public void setReason(int reason) {
+        this.reason = reason;
+    }
 }
