1bcb8510bbdbd1e9d1c7928389607724d15fd40e
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-20 11:59:25 +0300

    Install an appropriate version of jest

--- jouko-ui/jest.config.ts
+++ jouko-ui/jest.config.ts
@@ -1,14 +1,14 @@
 import type {Config} from '@jest/types';
 
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-};
-export default config;
-
 // Or async function
 export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: true,
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleNameMapper: {
+      '^.+\\.tsx?$': '<rootDir>/src',
+    },
   };
 };
