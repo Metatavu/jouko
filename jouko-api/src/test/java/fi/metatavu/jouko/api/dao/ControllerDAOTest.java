53ae63227721c402e5f02c02482a0d529e7cd209
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-22 14:45:20 +0300

    Check if a controller doesn't exist then null is returned

--- jouko-api/src/test/java/fi/metatavu/jouko/api/dao/ControllerDAOTest.java
+++ jouko-api/src/test/java/fi/metatavu/jouko/api/dao/ControllerDAOTest.java
@@ -16,7 +16,7 @@ public class ControllerDAOTest {
         
         // Create a new controller (String eui, String key, ControllerCommunicationChannel, communicationChannel)
         Mockito.when(controllerDAO.create(Mockito.anyString(), Mockito.anyString(), Mockito.any(ControllerCommunicationChannel.class)))
-                .thenReturn(new ControllerEntity(1l, "EUI", "KEY", ControllerCommunicationChannel.LORA));
+                .thenReturn(new ControllerEntity(1L, "EUI", "KEY", ControllerCommunicationChannel.LORA));
     }
 
     // Check that the newly created controller is returned
@@ -26,5 +26,12 @@ public class ControllerDAOTest {
         Assert.assertEquals("EUI", controller.getEui());
         Assert.assertEquals("KEY", controller.getKey());
         Assert.assertEquals(ControllerCommunicationChannel.LORA, controller.getCommunicationChannel());
+    }
+
+    // Check that no controller is returned when there is no controller with the given eui
+    @Test
+    public void testGetControllerByEui() {
+        ControllerEntity controller = controllerDAO.findByEui("EUI2131232132312");
+        Assert.assertNull(controller);
     }
 }
