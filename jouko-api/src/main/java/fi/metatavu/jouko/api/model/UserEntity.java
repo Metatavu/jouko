c45e616f5aca1f75c7fb9a671842ea26dcdfb9db
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-23 10:30:14 +0300

    Explain that certain entities create MySQL tables

--- jouko-api/src/main/java/fi/metatavu/jouko/api/model/UserEntity.java
+++ jouko-api/src/main/java/fi/metatavu/jouko/api/model/UserEntity.java
@@ -8,6 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Creates MySQL table "User" and sets data types for columns.
+ */
 @Table(name="User")
 @Entity
 public class UserEntity {
