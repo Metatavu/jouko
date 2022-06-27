e12bf92ef2ceedf4b646408dc6886e7bee4c4a77
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-27 12:58:13 +0300

    GprsMessageDAOTest find message by controller

--- jouko-api/src/test/java/fi/metatavu/jouko/api/dao/GprsMessageDAOTest.java
+++ jouko-api/src/test/java/fi/metatavu/jouko/api/dao/GprsMessageDAOTest.java
@@ -29,4 +29,20 @@ public class GprsMessageDAOTest {
         Assert.assertEquals(gprsMessage, gprsMessageDAO.findById(1L));
         System.out.println("Gprs message created");
     }
+
+    /**
+     * Find a gprs message by controller (ControllerEntity controller, long deviceId)
+     */
+    @Test
+    public void findGprsMessageByController() {
+        UserEntity user = new UserEntity(1L,"keycloakId", "name");
+        ControllerEntity controller = new ControllerEntity(1L, "EUI", "KEY", ControllerCommunicationChannel.GPRS);
+        DeviceEntity device = new DeviceEntity(1L, "Device", user, controller);
+        MessageType messageType = MessageType.NEW_INTERRUPTION;
+
+        GprsMessageEntity gprsMessage = gprsMessageDAO.create(controller, 1L, "test", messageType);
+        Mockito.when(gprsMessageDAO.findOneByController(controller, 1L)).thenReturn(gprsMessage);
+        Assert.assertEquals(gprsMessage, gprsMessageDAO.findOneByController(controller, 1L));
+        System.out.println("Gprs message found by controller");
+    }
 }
