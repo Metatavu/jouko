a91b422d88e9d734f25718488cbb50cc8cffca3a
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-27 13:41:19 +0300

    GprsMessageDAOTest create a message using a different type

--- jouko-api/src/test/java/fi/metatavu/jouko/api/dao/GprsMessageDAOTest.java
+++ jouko-api/src/test/java/fi/metatavu/jouko/api/dao/GprsMessageDAOTest.java
@@ -61,6 +61,22 @@ public class GprsMessageDAOTest {
         Assert.assertEquals(gprsMessage, gprsMessageDAO.findById(1L));
         gprsMessageDAO.delete(gprsMessage);
         Assert.assertNull(gprsMessageDAO.findById(1L));
-        System.out.println("Gprs message deleted");
+        System.out.println("Gprs message deleted (New interruption)");
+    }
+
+    /**
+     * Create a gprs message using a different message type (ControllerEntity controller, long deviceId, String content, MessageType messageType)
+     */
+    @Test
+    public void createGprsMessageWithDifferentMessageType() {
+        UserEntity user = new UserEntity(1L,"keycloakId", "name");
+        ControllerEntity controller = new ControllerEntity(1L, "EUI", "KEY", ControllerCommunicationChannel.GPRS);
+        DeviceEntity device = new DeviceEntity(1L, "Device", user, controller);
+        MessageType messageType = MessageType.CANCEL_INTERRUPTION;
+
+        GprsMessageEntity gprsMessage = gprsMessageDAO.create(controller, 1L, "test", messageType);
+        Mockito.when(gprsMessageDAO.findById(1L)).thenReturn(gprsMessage);
+        Assert.assertEquals(gprsMessage, gprsMessageDAO.findById(1L));
+        System.out.println("Gprs message created (Cancel interruption)");
     }
 }
