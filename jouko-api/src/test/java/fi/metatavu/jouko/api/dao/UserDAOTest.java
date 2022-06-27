3a4f22ebe4c8d23444a3e2ef6b9e17915d6b1c10
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-27 11:57:17 +0300

    Re-enable UserDAO tests

--- jouko-api/src/test/java/fi/metatavu/jouko/api/dao/UserDAOTest.java
+++ jouko-api/src/test/java/fi/metatavu/jouko/api/dao/UserDAOTest.java
@@ -6,32 +6,31 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-// Need to be able to delete a user or otherwise other tests will fail
-// public class UserDAOTest {
-//     UserDAO userDAO;
-//     UserEntity userEntity;
+public class UserDAOTest {
+    UserDAO userDAO;
+    UserEntity userEntity;
 
-//     @Before
-//     public void setUp() {
-//         userDAO = Mockito.mock(UserDAO.class);
-//     }
+    @Before
+    public void setUp() {
+        userDAO = Mockito.mock(UserDAO.class);
+    }
 
-//     /**
-//      * Create a new user
-//     */
-//     @Test
-//     public void testCreateUser() {
-//         UserEntity user = new UserEntity(
-//                 1L,
-//                 "keycloakId",
-//                 "name"
-//         );
+    /**
+     * Create a new user
+    */
+    @Test
+    public void testCreateUser() {
+        UserEntity user = new UserEntity(
+                1L,
+                "keycloakId",
+                "name"
+        );
 
-//         /**
-//          * Check that user exists
-//          */
-//         Mockito.when(userDAO.findById(1L)).thenReturn(user);
-//         Assert.assertEquals(user, userDAO.findById(1L));
-//         System.out.println("User created");
-//     }
-// }
+        /**
+         * Check that user exists
+         */
+        Mockito.when(userDAO.findById(1L)).thenReturn(user);
+        Assert.assertEquals(user, userDAO.findById(1L));
+        System.out.println("User created");
+    }
+}
