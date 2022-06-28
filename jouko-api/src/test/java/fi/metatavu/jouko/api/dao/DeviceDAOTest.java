395030858be6672592527c85366561975b1ae2f6
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-28 09:29:12 +0300

    List devices by user test

--- jouko-api/src/test/java/fi/metatavu/jouko/api/dao/DeviceDAOTest.java
+++ jouko-api/src/test/java/fi/metatavu/jouko/api/dao/DeviceDAOTest.java
@@ -1,16 +1,19 @@
 package fi.metatavu.jouko.api.dao;
 
-import fi.metatavu.jouko.api.model.ControllerCommunicationChannel;
-import fi.metatavu.jouko.api.model.ControllerEntity;
-import fi.metatavu.jouko.api.model.DeviceEntity;
-import fi.metatavu.jouko.api.model.UserEntity;
+import fi.metatavu.jouko.api.model.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+
 public class DeviceDAOTest {
     DeviceDAO deviceDAO;
+    InterruptionDAO interruptionDAO;
 
     @Before
     public void setUp() {
@@ -32,5 +35,22 @@ public class DeviceDAOTest {
         Mockito.when(deviceDAO.create(controller, "Device", user)).thenReturn(device);
         Assert.assertEquals(device, deviceDAO.create(controller, "Device", user));
         System.out.println("Device created");
+    }
+
+    /**
+     * List devices by user (UserEntity user, int firstResult, int maxResults)
+     */
+    @Test
+    public void testListDevicesByUser() {
+        UserEntity user = new UserEntity(
+                1L,
+                "keycloakId",
+                "name"
+        );
+        ControllerEntity controller = new ControllerEntity(1L, "EUI", "KEY", ControllerCommunicationChannel.LORA);
+        DeviceEntity device = new DeviceEntity(1L, "Device", user, controller);
+        Mockito.when(deviceDAO.listByUser(user, 0, 10)).thenReturn(Arrays.asList(device));
+        Assert.assertEquals(Arrays.asList(device), deviceDAO.listByUser(user, 0, 10));
+        System.out.println("Devices found by user");
     }
 }
