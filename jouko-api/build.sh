be5ca9b4389118c0f889603a7b147fbe016317d8
Otto Hooper <otto.hooper@metatavu.fi>
2022-06-29 14:28:03 +0300

    Remove docker from build.sh

--- jouko-api/build.sh
+++ jouko-api/build.sh
@@ -3,4 +3,3 @@
 mkdir -p target/generated-sources/protobuf/java
 protoc -I=./src/main/proto --java_out=./target/generated-sources/protobuf/java ./src/main/proto/laiteviestit.proto
 mvn package
-docker build -t jouko-api .
