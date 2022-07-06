dafd77a4de5050a956f0e2780e99ce7932a3ea7b
Otto Hooper <otto.hooper@metatavu.fi>
2022-07-06 14:45:13 +0300

    Change react config.ts to .local

--- jouko-admin-ui/src/config.ts
+++ jouko-admin-ui/src/config.ts
@@ -1,9 +1,9 @@
 // Configuration file for the application.
-// Java API URL (default: http://localhost:8080/v1)
+// Java API URL (default: http://dev.jouko.local:8080/v1)
 export const apiUrl =
   process.env.NODE_ENV === 'production'
   ? 'https://jouko-api.metatavu.io/v1'
-  : 'http://localhost:8080/v1';
+  : 'http://dev.jouko.local:8080/v1';
 
 // Keycloak auth URL (default: http://localhost:9080/auth)
 export const authUrl =
@@ -15,7 +15,7 @@ export const authUrl =
 export const uploadUrl =
   process.env.NODE_ENV === 'production'
   ? 'https://jouko-api.metatavu.io/fileUpload'
-  : 'http://localhost:8080/fileUpload';
+  : 'http://dev.jouko.local:8080/fileUpload';
 
 // Keycloak realm name, if you followed the README instructions, this should be "jouko-realm"
 export const keycloakRealm =
